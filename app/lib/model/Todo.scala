99749616ff23b6fe5c5eaaf04168dab08ca2fc1f
Takeaki <takeaki.yamamto@gmail.com>
2022-12-15 10:48:16 +0900

    modelとrepositorの定義￥

---
+++ app/lib/model/Todo.scala
@@ -0,0 +1,56 @@
+/**
+  * This is a sample of Todo Application.
+  * 
+  */
+
+package lib.model
+
+import ixias.model._
+import ixias.util.EnumStatus
+
+import java.time.LocalDateTime
+
+// ユーザーを表すモデル
+//~~~~~~~~~~~~~~~~~~~~
+import Todo._
+case class Todo(
+  id:           Option[Id],
+  category_id:  Long, 
+  title:        String,
+  body:         String,
+  state:        Status,
+  updatedAt:    LocalDateTime = NOW,
+  createdAt:    LocalDateTime = NOW
+) extends EntityModel[Id]
+
+// コンパニオンオブジェクト
+//~~~~~~~~~~~~~~~~~~~~~~~~
+object Todo {
+
+  val  Id = the[Identity[Id]]
+  type Id = Long @@ Todo
+  type WithNoId = Entity.WithNoId [Id, Todo]
+  type EmbeddedId = Entity.EmbeddedId[Id, Todo]
+
+  // ステータス定義
+  //~~~~~~~~~~~~~~~~~
+  sealed abstract class Status(val code: Short, val name: String) extends EnumStatus
+  object Status extends EnumStatus.Of[Status] {
+    case object TODO        extends Status(code = 0,   name = "TODO")
+    case object IN_PROGRESS extends Status(code = 100, name = "進行中")
+    case object DONE        extends Status(code = 200, name = "完了")
+  }
+
+  // INSERT時のIDがAutoincrementのため,IDなしであることを示すオブジェクトに変換
+  def apply(category_id: Int, title: String, body: String, state: Status): WithNoId = {
+    new Entity.WithNoId(
+      new Todo(
+        id          = None,
+        category_id = category_id, 
+        title       = title, 
+        body        = body, 
+        state       = state
+      )
+    )
+  }
+}
