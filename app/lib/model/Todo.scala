7b70691234f0de4daae632669724d28cf284ac28
Takeaki <takeaki.yamamto@gmail.com>
2022-12-20 19:10:30 +0900

    todo_categoryのモデルとdb query

--- app/lib/model/Todo.scala
+++ app/lib/model/Todo.scala
@@ -10,7 +10,7 @@ import ixias.util.EnumStatus
 
 import java.time.LocalDateTime
 
-// ユーザーを表すモデル
+// Todoを表すモデル
 //~~~~~~~~~~~~~~~~~~~~
 import Todo._
 case class Todo(
@@ -20,6 +20,16 @@ case class Todo(
   body:         String,
   state:        Status,
   updatedAt:    LocalDateTime = NOW,
+  createdAt:    LocalDateTime = NOW
+) extends EntityModel[Id]
+
+import  TodoCategory._
+case class TodoCategory(
+  id:           Option[Id], 
+  name:         String, 
+  slug:         String, 
+  categoryColor:Color, 
+  updatedAt:    LocalDateTime = NOW, 
   createdAt:    LocalDateTime = NOW
 ) extends EntityModel[Id]
 
@@ -50,6 +60,34 @@ object Todo {
         title       = title, 
         body        = body, 
         state       = state
+      )
+    )
+  }
+}
+
+object TodoCategory {
+  val  Id = the[Identity[Id]]
+  type Id = Long @@ Todo
+  type WithNoId = Entity.WithNoId [Id, TodoCategory]
+  type EmbeddedId = Entity.EmbeddedId[Id, TodoCategory]
+
+  // ステータス定義
+  //~~~~~~~~~~~~~~~~~
+  sealed abstract class Color(val code: Short, val name: String) extends EnumStatus
+  object Color extends EnumStatus.Of[Color] {
+    case object TODO        extends Color(code = 0, name = "red")
+    case object IN_PROGRESS extends Color(code = 1, name = "blue")
+    case object DONE        extends Color(code = 2, name = "breen")
+  }
+
+  // INSERT時のIDがAutoincrementのため,IDなしであることを示すオブジェクトに変換
+  def apply(name: String, slug: String, categoryColor: Color): WithNoId = {
+    new Entity.WithNoId(
+      new TodoCategory(
+        id          = None,
+        name        = name, 
+        slug        = slug, 
+        categoryColor = categoryColor, 
       )
     )
   }
--- app/lib/persistence/db/Todo.scala
+++ app/lib/persistence/db/Todo.scala
@@ -10,6 +10,7 @@ import slick.jdbc.JdbcProfile
 import ixias.persistence.model.Table
 
 import lib.model.Todo
+import lib.model.TodoCategory
 
 // TodoTable: Todoテーブルへのマッピングを行う
 //~~~~~~~~~~~~~~
@@ -55,6 +56,54 @@ case class TodoTable[P <: JdbcProfile]()(implicit val driver: P)
       // Model => Tuple(table)
       (v: TableElementType) => Todo.unapply(v).map { t => (
         t._1, t._2, t._3, t._4, t._5, LocalDateTime.now(), t._7
+      )}
+    )
+  }
+}
+
+// TodoTable: Todoテーブルへのマッピングを行う
+//~~~~~~~~~~~~~~
+case class TodoCategoryTable[P <: JdbcProfile]()(implicit val driver: P)
+  extends Table[TodoCategory, P] {
+  import api._
+
+  // Definition of DataSourceName
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  lazy val dsn = Map(
+    "master" -> DataSourceName("ixias.db.mysql://master/to_do_category"),
+    "slave"  -> DataSourceName("ixias.db.mysql://slave/to_do_category")
+  )
+
+  // Definition of Query
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  class Query extends BasicQuery(new Table(_)) {}
+  lazy val query = new Query
+
+  // Definition of Table
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  class Table(tag: Tag) extends BasicTable(tag, "to_do_category") {
+    import TodoCategory._
+    // Columns
+    /* @1 */ def id            = column[Id]            ("id",              O.UInt64, O.PrimaryKey, O.AutoInc)
+    /* @3 */ def name          = column[String]        ("name",            O.Utf8Char255)
+    /* @4 */ def slug          = column[String]        ("slug",            O.Utf8Char255)
+    /* @5 */ def categoryColor = column[Color]         ("category_color",  O.UInt8)
+    /* @6 */ def updatedAt     = column[LocalDateTime] ("updated_at",      O.TsCurrent)
+    /* @7 */ def createdAt     = column[LocalDateTime] ("created_at",      O.Ts)
+
+    type TableElementTuple = (
+      Option[Id], String, String, Color, LocalDateTime, LocalDateTime
+    )
+
+    // DB <=> Scala の相互のmapping定義
+    def * = (id.?, name, slug, categoryColor, updatedAt, createdAt) <> (
+      // Tuple(table) => Model
+      (t: TableElementTuple) => TodoCategory(
+        t._1, t._2, t._3, t._4, t._5, t._6
+      ),
+      // Model => Tuple(table)
+      (v: TableElementType) => TodoCategory.unapply(v).map { t => (
+        t._1, t._2, t._3, t._4, LocalDateTime.now(), t._6
       )}
     )
   }
