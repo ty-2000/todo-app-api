fb16cec8d51b0ff9e0ac2b86dddde3632bc2ccb5
Takeaki <takeaki.yamamto@gmail.com>
2023-01-12 11:16:29 +0900

    Category一覧ページの作成

---
+++ app/controllers/Category.scala
@@ -0,0 +1,39 @@
+/**
+ *
+ * to do sample project
+ *
+ */
+
+package controllers
+
+import javax.inject._
+import play.api.i18n.I18nSupport
+import play.api.mvc._
+
+import scala.concurrent.ExecutionContext.Implicits.global
+
+import model.ViewValueHome
+
+import lib.persistence.onMySQL._ // repository
+import lib.model.TodoCategory
+
+
+@Singleton
+class CategoryController @Inject()(val controllerComponents: ControllerComponents) extends BaseController with I18nSupport {
+
+  def index() = Action.async { implicit req =>
+    val vv = ViewValueHome(
+      title = "Todo一覧", 
+      cssSrc = Seq("main.css"), 
+      jsSrc  = Seq("main.js"), 
+    )
+    val getAllCategoryFuture = TodoCategoryRepository.getAll()
+
+    for {
+      categorySeq         <- getAllCategoryFuture
+    } yield {
+      Ok(views.html.Category(vv, categorySeq))
+    }
+  }
+}
+
